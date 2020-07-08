8d097d6dc6cd2e31324f8e244bac4ad5812fe6c4
Renato Costa <renato.costa@jimdo.com>
2020-07-08 10:20:24 +0200

    pipeline debug information

--- src/main/kotlin/coroutines/devcop/iv_patterns/2_2_pipeline_primes.kt
+++ src/main/kotlin/coroutines/devcop/iv_patterns/2_2_pipeline_primes.kt
@@ -4,21 +4,30 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.runBlocking
 import util.trace
 
 @ExperimentalCoroutinesApi
 private fun CoroutineScope.createNumberProducer(startFrom: Int): ReceiveChannel<Int> = produce<Int> {
     var x = startFrom
-    while (true) {
+    while (isActive) {
+        delay(1_000)
+        trace("sending $x")
         send(x++)
     }
 }
 
 @ExperimentalCoroutinesApi
-private fun CoroutineScope.createFilterProducer(numberChannel: ReceiveChannel<Int>, prime: Int): ReceiveChannel<Int> =
+private fun CoroutineScope.createFilterProducer(
+    numberChannel: ReceiveChannel<Int>,
+    prime: Int,
+    producerId: Int
+): ReceiveChannel<Int> =
     produce {
         for (number in numberChannel) {
+            trace("[$producerId] Comparing $number mod $prime")
             if (number % prime != 0) {
                 send(number)
             }
@@ -28,10 +37,10 @@ private fun CoroutineScope.createFilterProducer(numberChannel: ReceiveChannel<In
 @ExperimentalCoroutinesApi
 fun main() = runBlocking<Unit>(Dispatchers.Default) {
     var primeProducer = createNumberProducer(2)
-    repeat(30) {
+    repeat(30) { producerId ->
         val prime = primeProducer.receive()
         trace("Prime $prime")
-        primeProducer = createFilterProducer(primeProducer, prime)
+        primeProducer = createFilterProducer(primeProducer, prime, producerId)
     }
     trace("Done")
     this.coroutineContext.cancelChildren()
