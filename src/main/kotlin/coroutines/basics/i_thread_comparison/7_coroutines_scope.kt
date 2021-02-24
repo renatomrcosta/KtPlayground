2f5ec635bb17d0b8c9dcc627c4847c53be5c4733
Renato Costa <renato.rezende.costa@gmail.com>
2021-02-24 20:56:52 +0100

    scopes shenanigans

--- src/main/kotlin/coroutines/basics/i_thread_comparison/7_coroutines_scope.kt
+++ src/main/kotlin/coroutines/basics/i_thread_comparison/7_coroutines_scope.kt
@@ -1,19 +1,38 @@
 package coroutines.basics.i_thread_comparison
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import util.trace
 import util.withExecutionTime
+import java.util.concurrent.Executors
+
 
 fun main() = withExecutionTime {
+    val myDispatcher =
+        Executors.newFixedThreadPool(10).asCoroutineDispatcher()
+
     runBlocking {
-        repeat(100_000) {
-            launch {
-                delay(1000)
-                println("Executed Job #$it")
+        launch(Dispatchers.IO) {
+            repeat(100_000) {
+                launch {
+                    delay(1000)
+                    trace("Executed Job #$it")
+                }
+            }
+        }
+        launch(myDispatcher) {
+            repeat(100_000) {
+                launch {
+                    delay(1000)
+                    trace("Executed Job #$it")
+                }
             }
         }
     }
+    myDispatcher.close()
 }
 
 
